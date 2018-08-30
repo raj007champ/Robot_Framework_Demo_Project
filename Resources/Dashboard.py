1601b4c886b8d9e0d93b648472a253a534b821d4
shishu.raj <shishu.pandey@msystechnologies.com>
2018-08-30 13:56:48 +0530

    Minor changes

--- Resources/Dashboard.py
+++
@@ -1,6 +0,0 @@
-from selenium import webdriver
-from selenium.webdriver.common.by import By
-
-def verify_dashboard():
-    getText = driver.find_element(By.XPATH, '(//div[@class="ant-breadcrumb"]/span/span)[1]').text
-    print(getText)
